e41cfecae09e2fd2753945c0c93a15f3bf1758f1
tope <topeojewale@gmail.com>
2020-03-07 20:08:56 +0100

    worked on states, props and added .env file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import React, {Component} from "react";
-import Navbar from './components/layout/Navbar'
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
-import Useritem from "./components/user/Useritem";
-
+import Users from "./components/user/Users";
+import axios from "axios";
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+      console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
+    const { users, loading } = this.state;
     return (
-        <div className="App">
-            <Navbar title="Github Finder"/>
-            <Useritem/>
-          </div>
-  )
+      <div className="App">
+        <Navbar title="Github Finder" />
+        <div className="container">
+          <Users loading={loading} users={users} />
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,18 +1,16 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-class Navbar extends Component {
-    static propTypes = {
-        title: PropTypes.string.isRequired
-    }
-    render() {
-        return (
-            <nav className="navbar bg-primary">
-                <h1>{this.props.title}</h1>
-            </nav>
-    )
-        ;
-    }
-}
+const Navbar = ({title})=> {
 
+    return (
+      <nav className="navbar bg-primary">
+        <h1>{title}</h1>
+      </nav>
+    );
+
+};
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired
+};
 export default Navbar;
