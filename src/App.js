192c4365b2e775792ef3e4f2f2c8e115d34a3799
tope <topeojewale@gmail.com>
2020-03-30 21:56:44 +0100

    refactored class based components to functiona components and added context for state management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import "./App.css";
@@ -8,90 +8,109 @@ import Seacrh from "./components/user/Seacrh";
 import Alert from "./components/user/Alert";
 import About from "./components/pages/About";
 import User from "./components/user/User";
-class App extends Component {
-  state = {
-    users: [],
-    user: {},
-    loading: false,
-    alert: null
-  };
+import GithubState from "./context/github/GithubState";
+
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
+
   // async componentDidMount() {
   //   console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
-  //   this.setState({ loading: true });
+  //   setState({ loading: true });
   //   const res = await axios.get(
   //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
   //   );
-  //   this.setState({ users: res.data, loading: false });
+  //   setState({ users: res.data, loading: false });
   // }
-  searchUsers = async searchQuery => {
-    this.setState({ loading: true });
+  const searchUsers = async searchQuery => {
+    setLoading( true );
     const res = await axios.get(
       `https://api.github.com/search/users?q=${searchQuery}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data.items, loading: false });
+    setUsers(res.data.items);
+    setLoading(false)
   };
 
   // get a single user
-  getUser = async username => {
-    this.setState({ loading: true });
+ const getUser = async username => {
+    setLoading(true );
     const res = await axios.get(
       `https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ user: res.data, loading: false });
+    setUser( res.data);
   };
+
+  // get user repo
+  const getUserRepo = async username => {
+   setLoading(true);
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    setRepos(res.data);
+    setLoading(true);
+  };
+
   // clear users from state
-  clearUsers = () => {
-    this.setState({ users: [], loading: false });
+  const clearUsers = () => {
+    setUsers([]);
+    setLoading(false)
   };
-  setAlert = (message, type) => {
-    this.setState({ alert: { message, type } });
+  const setAlerts = (message, type) => {
+    setAlert( { message, type });
     setTimeout(() => {
-      this.setState({ alert: null });
+      setAlert(null)
     }, 5000);
   };
-  render() {
-    const { users, loading, alert, user } = this.state;
+
     return (
-      <Router>
-        <div className="App">
-          <Navbar title="Github Finder" />
-          <div className="container">
-            <Alert alert={alert} />
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={props => (
-                  <Fragment>
-                    <Seacrh
-                      searchUsers={this.searchUsers}
-                      clearUsers={this.clearUsers}
-                      showClearBtn={users.length > 0}
-                      setAlert={this.setAlert}
-                    />
-                    <Users loading={loading} users={users} />
-                  </Fragment>
-                )}
-              />
-              <Route exact path="/about" component={About} />
-              <Route
-                exact
-                path="/user/:login"
-                render={props => (
-                  <User
-                    {...props}
-                    getUser={this.getUser}
-                    user={user}
-                    loading={loading}
+        <GithubState>
+          <Router>
+            <div className="App">
+              <Navbar title="Github Finder" />
+              <div className="container">
+                <Alert alert={alert} />
+                <Switch>
+                  <Route
+                      exact
+                      path="/"
+                      render={props => (
+                          <Fragment>
+                            <Seacrh
+                                searchUsers={searchUsers}
+                                clearUsers={clearUsers}
+                                showClearBtn={users.length > 0}
+                                setAlert={setAlerts}
+                            />
+                            <Users loading={loading} users={users} />
+                          </Fragment>
+                      )}
                   />
-                )}
-              />
-            </Switch>
-          </div>
-        </div>
-      </Router>
+                  <Route exact path="/about" component={About} />
+                  <Route
+                      exact
+                      path="/user/:login"
+                      render={props => (
+                          <User
+                              {...props}
+                              getUser={getUser}
+                              user={user}
+                              getUserRepos = {getUserRepo}
+                              repos = {repos}
+                              loading={loading}
+                          />
+                      )}
+                  />
+                </Switch>
+              </div>
+            </div>
+          </Router>
+        </GithubState>
     );
-  }
-}
+
+};
 
 export default App;
--- src/components/user/Seacrh.js
+++ src/components/user/Seacrh.js
@@ -1,56 +1,48 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-class Seacrh extends Component {
-  state = {
-    searchQuery: ""
+
+const Seacrh = ({ showClearBtn, clearUsers, searchUsers, setAlert }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const onInputChange = e => {
+    setSearchQuery(e.target.value );
   };
-  onInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-  search = e => {
-      const {searchQuery} = this.state;
+  const search = e => {
     e.preventDefault();
-    if (searchQuery === '') {
-        this.props.setAlert('Please enter a search term', 'light')
+    if (searchQuery === "") {
+      setAlert("Please enter a search term", "light");
     } else {
-        this.props.searchUsers(searchQuery);
-        this.setState({ searchQuery: "" });
+      searchUsers(searchQuery);
+      setSearchQuery( "" );
     }
   };
-  render() {
-    const { searchQuery } = this.state;
-    const {showClearBtn, clearUsers} = this.props;
-    return (
-      <div>
-        <form className="form" onSubmit={this.search}>
-          <input
-            type="text"
-            name="searchQuery"
-            placeholder="Search Users"
-            value={searchQuery}
-            onChange={this.onInputChange}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-          {showClearBtn && <button
-              className="btn btn-light btn-block"
-              onClick={clearUsers}
-          >
-              Clear
-          </button>}
-
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <form className="form" onSubmit={search}>
+        <input
+          type="text"
+          name="searchQuery"
+          placeholder="Search Users"
+          value={searchQuery}
+          onChange={onInputChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {showClearBtn && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 Seacrh.propTypes = {
   searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClearBtn: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClearBtn: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired
 };
 export default Seacrh;
